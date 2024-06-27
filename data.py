e8fdb53dd56d68e00f3cc837bedbbd33121b0042
Anastasiia Trusova <trusovaanastasiaa@gmail.com>
2024-06-27 23:28:57 +0200

    Sprint 7

---
+++ data.py
@@ -0,0 +1,51 @@
+class OrderData:
+    order_data_black_color = {
+        "firstName": "Anastasiia",
+        "lastName": "Ivanova",
+        "address": "Gorkogo 4",
+        "metroStation": 4,
+        "phone": "+7 8097875577",
+        "rentTime": 10,
+        "deliveryDate": "2024-09-01",
+        "comment": "Call before delivery",
+        "color": ["BLACK"]
+    }
+
+    order_data_gray_color = {
+        "firstName": "Anastasiia",
+        "lastName": "Ivanova",
+        "address": "Gorkogo 4",
+        "metroStation": 4,
+        "phone": "+7 8097875577",
+        "rentTime": 10,
+        "deliveryDate": "2024-09-01",
+        "comment": "Call before delivery",
+        "color": ["GREY"]
+    }
+
+    order_data_two_color = {
+        "firstName": "Anastasiia",
+        "lastName": "Ivanova",
+        "address": "Gorkogo 4",
+        "metroStation": 4,
+        "phone": "+7 8097875577",
+        "rentTime": 10,
+        "deliveryDate": "2024-09-01",
+        "comment": "Call before delivery",
+        "color": ["BLACK", "GREY"]
+    }
+
+    order_data_without_color = {
+        "firstName": "Anastasiia",
+        "lastName": "Ivanova",
+        "address": "Gorkogo 4",
+        "metroStation": 4,
+        "phone": "+7 8097875577",
+        "rentTime": 10,
+        "deliveryDate": "2024-09-01",
+        "comment": "Call before delivery",
+        "color": []
+    }
+
+    result_code = 201
+    result_text = ''
